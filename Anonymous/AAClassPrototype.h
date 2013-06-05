3ea91299a3ccd6f0db7b6862422cdd9d89f65654
Adam Sharp <adam.sharp@outware.com.au>
2013-06-05 17:27:52 +1000

    Add API docs for AAClassPrototype

--- Anonymous/AAClassPrototype.h
+++ Anonymous/AAClassPrototype.h
@@ -8,9 +8,33 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ Provides an object-oriented API to the Objective-C runtime for creating
+ "anonymous" classes that implement an existing interface. The class
+ can be used to instantiate a new object that conforms to a protocol.
+
+ The classes that are generated are anonymous in that they are
+ dynamically allocated and registered with the runtime, and their
+ lifetime is restricted to the lifetime of the object instantiated from
+ the prototype.
+ */
 @interface AAClassPrototype : NSObject
 
+/**
+ Create a class prototype based on the given protocol.
+
+ @param protocol The protocol to base the class prototype on.
+ */
 - (id)initWithProtocol:(Protocol *)protocol;
+
+/**
+ Get an instance of the prototype. This should only be called once.
+ Instantiating a second instance from a prototype is illegal and will
+ raise an exception.
+
+ @return An instance of a new anonymous NSObject subclass described by
+     this prototype.
+ */
 - (id)new;
 
 @property (nonatomic, readonly) Protocol *protocol;
